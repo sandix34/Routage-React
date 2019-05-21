b2a7d975b325dd86e60c684d67324bcb45429964
sandrine <sandrinezocca@gmail.com>
2019-05-21 17:19:05 +0200

    effectuer une requête AJAX

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      Le routage
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    todos: []
+  }
 
+  // Appel de la méthode effectuant la requête Ajax
+  componentDidMount() {
+    this.getTodos()
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          todos: data
+        })
+      })
+  }
+
+  // méthode effectuant une requpete AJAX 
+  // Fausse API REST en ligne --> https://jsonplaceholder.typicode.com/
+  getTodos() {
+    return fetch('https://jsonplaceholder.typicode.com/todos');
+  }
+  
+  render() {
+    console.log(this.state.todos);
+     
+    return ( 
+      <div className="App">
+        <header className="App-header">
+        Le routage
+        </header>
+      </div>
+     );
+  }
+}
+ 
 export default App;
+
